d9cb4606481148e5f44ec224a3e76cce01e31bd8
MageFox <sample@test.com>
2021-01-22 10:15:16 -0600

    Hola Mundo

--- ElRecopilado/ElRecopilado/Program.cs
+++ ElRecopilado/ElRecopilado/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ElRecopilado.Tarea;
+using System;
 
 namespace ElRecopilado
 {
@@ -6,7 +7,24 @@ namespace ElRecopilado
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Elije opcion:");
+            Console.WriteLine("1 Hola Mundo");
+            String texto;
+            texto = Console.ReadLine();
+
+            switch (texto)
+            {
+                case "1":
+                    HolaMundo holaMundoObj = new HolaMundo();
+                    holaMundoObj.HacerMagia();
+                    break;
+                default:
+                    Console.WriteLine("Opcion invalida");
+                    Console.WriteLine("Adios");
+                    break;
+            }
+
+
         }
     }
 }
---
+++ ElRecopilado/ElRecopilado/Tarea/HolaMundo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElRecopilado.Tarea
+{
+    public class HolaMundo
+    {
+        public void HacerMagia()
+        {
+            Console.WriteLine("Karim Rodrigo");
+            Console.WriteLine("matricula");
+            Console.WriteLine("karin.elayan");
+        }
+    }
+}
