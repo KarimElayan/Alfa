c9a74f88bc5b3d6cef1dab16345bdf0720c3a487
Gustavo <gustavo.hernandez795@gmail.com>
2021-01-24 14:13:31 -0600

    Cambio de datos

---
+++ ElRecopilado/ElRecopilado/Tarea/HolaMundo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElRecopilado.Tarea
+{
+    public class HolaMundo
+    {
+        public void HacerMagia()
+        {
+            Console.WriteLine("Gustavo Adolfo Bautista Hernandez");
+            Console.WriteLine("matricula: cuh51197227");
+            Console.WriteLine("Correo: cuh51197227@cuh.mx");
+           
+        }
+    }
+}
