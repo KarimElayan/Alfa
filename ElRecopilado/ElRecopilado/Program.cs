39e6aa318bb7601a3d4dcefcca81e59b29fbde2a
Gustavo <gustavo.hernandez795@gmail.com>
2021-01-24 10:29:53 -0600

    Revert "Hola Mundo"
    This reverts commit d9cb4606481148e5f44ec224a3e76cce01e31bd8.

--- ElRecopilado/ElRecopilado/Program.cs
+++ ElRecopilado/ElRecopilado/Program.cs
@@ -1,5 +1,4 @@
-﻿using ElRecopilado.Tarea;
-using System;
+﻿using System;
 
 namespace ElRecopilado
 {
@@ -7,24 +6,7 @@ namespace ElRecopilado
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Elije opcion:");
-            Console.WriteLine("1 Hola Mundo");
-            String texto;
-            texto = Console.ReadLine();
-
-            switch (texto)
-            {
-                case "1":
-                    HolaMundo holaMundoObj = new HolaMundo();
-                    holaMundoObj.HacerMagia();
-                    break;
-                default:
-                    Console.WriteLine("Opcion invalida");
-                    Console.WriteLine("Adios");
-                    break;
-            }
-
-
+            
         }
     }
 }
