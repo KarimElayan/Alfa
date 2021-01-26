df2bdba7e78a7b1ba6bbd2abd89b51a05f18f276
LAPTOP-RBAM611A\mari2 <mari.22aguilar>
2021-01-25 20:29:33 -0600

    Hola_mundo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********Hola Mundo********");
+            Console.WriteLine("Mi nombre es: Marisol Aguilar Vargas");
+            Console.WriteLine("Tengo 20 años");
+            Console.WriteLine("Voy en el grupo 19A");
+            Console.WriteLine("Mi numero de matricula es: CUH51197332");
+        }
+    }
+}
