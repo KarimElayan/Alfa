e45713c6eb7b4b0052d6043a456be409fec1a9c2
Magaly_51197235 <cuh51197235@cuh.mx>
2021-01-26 14:09:16 -0600

    Practica 5, Practica 6

---
+++ ElRecopilado/ElRecopilado/Tarea/Practica 5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Practica_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numero;
+            //se dice que tiene 5 filas y 5 columnas
+            numero = new int[5, 5];
+            Random aleatorio = new Random();
+            //valores de la matriz 
+            for (int fila = 0; fila < 5; fila++)
+            {
+                for (int columna = 0; columna < 5; columna++)
+                {
+                    {
+                        numero[fila, columna] = aleatorio.Next(-50, 50);
+                    }
+                    Console.WriteLine("Ingresa un numero solo del 2 al 100:  ");
+                    numero[fila, columna] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+            Console.Clear();
+            //hacer la matriz en pantalla para usuario
+            Console.WriteLine("--------------")
+                Console.writeLine("Matriz usuario")
+            for (int fila = 0; fila < 5; fila++)
+            {
+                for (int columna = 0; columna < 5; columna++)
+                {
+                    Console.Write(" " + numero[fila, columna]);
+                }
+                Console.WriteLine();
+            }
+            //hacer la matriz en pantalla para programador 
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Matriz generada");
+            for (int fila = 0; fila < 5; fila++)
+            {
+                for (int columna = 0; columna < 5; columna++)
+                {
+                    Console.Write(numero[fila, columna]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ElRecopilado/ElRecopilado/Tarea/Practica 6.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+            Random rnd = new Random();
+            for (int i = 0; i < 3; i++) ;
+            {
+                for (int j = 0; j < 3; j++) ;
+                {
+                    matriz[i, j] = rnd.Next(2, 9);
+                    Console.Write(matriz[i, j] + "");
+                }
+                Console.Write("/n");
+            }
+            Console.WriteLine("--------------")
+                Console.WriteLine("diagonal derecha")
+                for (int i = 0; i < 3; i++) ;
+            {
+                for (int j = 0; j < 3; j++) ;
+                {
+                    if (j == (3 - 1) - 1)
+                        int[] array = new int[3];
+                    Array[i] = matriz[i, j];
+                    Console.Write(Array[i] + "");
+                }
+                Console.WriteLine("--------------")
+                Console.WriteLine("diagonal izquierda")
+                    for (int i = 0; i < 3; i++) ;
+                {
+                    for (int j = 0; j < 3; j++) ;
+                    {
+                        if (j == (3 - 1) - 1)
+                            int[] array = new int[3];
+                        Array[i] = matriz[i, j];
+                        Console.Write(Array[i] + "");
+                    }
+                }
+            }
+        }
+    }
+}
