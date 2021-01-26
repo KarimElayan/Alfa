0357cd764a054118787ea28643b64e94525ed39c
MageFox <sample@test.com>
2021-01-25 18:37:55 -0600

    Temas del dia

---
+++ ElRecopilado/ElRecopilado/EnClase/Colores.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElRecopilado.EnClase
+{
+    class Colores
+    {
+        public int color;
+        public float grosor;
+        public bool hasPunta;
+        public string ToString()
+        {
+            return "[color: "+ color +"grosor: " + grosor + "hasPunta" + hasPunta+ "]";
+        }
+    }
+}
--- ElRecopilado/ElRecopilado/Program.cs
+++ ElRecopilado/ElRecopilado/Program.cs
@@ -1,4 +1,5 @@
-﻿using ElRecopilado.Tarea;
+﻿using ElRecopilado.EnClase;
+using ElRecopilado.Tarea;
 using System;
 
 namespace ElRecopilado
@@ -6,6 +7,13 @@ namespace ElRecopilado
     class Program
     {
         static void Main(string[] args)
+        {
+            MenuClase();
+            //MenuTarea();
+
+
+        }
+        private static void MenuTarea()
         {
             Console.WriteLine("Elije opcion:");
             Console.WriteLine("1 Hola Mundo");
@@ -23,8 +31,30 @@ namespace ElRecopilado
                     Console.WriteLine("Adios");
                     break;
             }
+        }
+        private static void MenuClase()
+        {
+            int opc = 2;
+            String texto = "Prueba de colores";
+            Console.WriteLine($"Se ejecuta la opc: {opc}) {texto}");
+            
+            texto = Console.ReadLine();
 
-
+            switch (opc)
+            {
+                case 1:
+                    HolaMundo holaMundoObj = new HolaMundo();
+                    holaMundoObj.HacerMagia();
+                    break;
+                case 2:
+                    UsarColores usarColores = new UsarColores();
+                    usarColores.HacerMagia();
+                    break;
+                default:
+                    Console.WriteLine("Opcion invalida");
+                    Console.WriteLine("Adios");
+                    break;
+            }
         }
     }
 }
