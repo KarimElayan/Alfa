a3094e660bb2c71b3c8ae77efe798e87dc559783
Gustavo <gustavo.hernandez795@gmail.com>
2021-01-25 22:35:16 -0600

    Practicas 5 y 6

---
+++ ElRecopilado/ElRecopilado/Tarea/Practica_Num6_GustavoAdolfoBautistaHernandez.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Practica6
+{
+    public class Practica6   
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[3, 3];
+            int SUMIzq;
+            //int SUMDer;  //variable para sumar la linea derecha 
+            // int Resta; //variable para restar la linea izquierda y derecha de la matriz 
+           // int C = 0;
+            
+            Console.WriteLine("******************************************");
+            Console.WriteLine("      Matriz de usuario de 3 x 3          ");
+            Console.WriteLine();
+            Console.WriteLine("******************************************");
+            Console.WriteLine();
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Console.Write("Ingresa el dato del indice [" + c + "," + f + "]: ");
+                    matriz [f, c] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Los Datos que ingresaste son: ");
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Console.Write(matriz [f, c] + "," + " " );
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            SUMIzq = 0;
+            for (int f = 0; f < 3; f++)
+            {
+                SUMIzq = SUMIzq +  matriz[f, f];
+            }
+            Console.WriteLine("La suma de la diagonal izquierda es  :" + SUMIzq);
+            Console.WriteLine();
+
+            //En esta parte quice sumar la linea de la matriz en la derecha pero no me compilaba y me daba error en la suma no se a que se deba
+            // lo intente pero ni me funciono
+             //SUMDer = 0;
+             //C = 3;
+             //for (int c = 1; c <= 3; c++)
+             //{
+               //SUMDer = SUMDer + matriz [c, C ];
+               //C = C - 1;
+              //}
+             //Console.WriteLine("La suma de la diagonal derecha es  :" + SUMDer);
+             //Resta = SUMIzq - SUMDer;
+            //Console.WriteLine("La resta de las dos diagonales (izquierda y derecha) es: " + Resta);
+
+
+
+        }
+           
+    }
+}
---
+++ ElRecopilado/ElRecopilado/Tarea/Practica_Num_5_GustavoAdolfoBautistaHernandez.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp12practica5
+{
+    class Practica5
+    {
+        static void Main(string[] args)
+        {
+            int indiceFila, indiceCol, tamFilasCol;
+            Console.WriteLine("Matriz De usuario");
+            Console.WriteLine("Favor de usar un rango entre el 2 y el 100 para usar la matriz");
+            Console.WriteLine("y");
+            Console.WriteLine("Tambien usar un rango de numeros entre el -50 y el 50 para usar el programa");
+
+            Console.WriteLine("¿Cuantas Filas y Columnas tendra tu matriz?");
+            tamFilasCol = Convert.ToInt32(Console.ReadLine());
+
+
+            int[,] matrizUsuario = new int[tamFilasCol, tamFilasCol];
+
+            if (tamFilasCol >= 2 && tamFilasCol <= 100)
+            {
+
+                for (indiceFila = 0; indiceFila < tamFilasCol; indiceFila++)
+                {
+                    for (indiceCol = 0; indiceCol < tamFilasCol; indiceCol++)
+                    {
+                        Console.Write("Ingresa el dato del indice [" + indiceFila + "," + indiceCol + "]: ");
+                        matrizUsuario[indiceFila, indiceCol] = Convert.ToInt32(Console.ReadLine());
+                         
+                    }
+                }
+
+
+                Console.WriteLine("Los Datos que ingresaste son: ");
+                for (indiceFila = 0; indiceFila < tamFilasCol; indiceFila++)
+                {
+                    for (indiceCol = 0; indiceCol < tamFilasCol; indiceCol++)
+                    {
+                        Console.Write(matrizUsuario[indiceFila, indiceCol] +"," + " ");
+
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.Write("Numero Fuera de Rango. Solo Rangos del 2 al 100. Vuelve a Iniciar el programa.");
+            };
+
+            
+
+        }
+    }
+}
