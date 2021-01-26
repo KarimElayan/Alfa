d0051913d5b661e6090d0190575ae614e0bc3a9b
MageFox <sample@test.com>
2021-01-26 09:20:06 -0600

    Pilas y Colas Init

---
+++ ElRecopilado/ElRecopilado/EnClase/Cola.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElRecopilado.EnClase
+{
+    class Cola
+    {
+        public void HacerMagia()
+        {
+            Queue<string> my_queue = new Queue<string>();
+
+            my_queue.Enqueue("rojo");
+            my_queue.Enqueue("azul");
+            my_queue.Enqueue("negro");
+            my_queue.Enqueue("amarillo");
+
+            Console.WriteLine("Total de elements en my_queue: {0}", my_queue.Count);
+
+            Console.WriteLine("REMOVER el primer elemento de la cola: {0}", my_queue.Dequeue());
+
+
+            Console.WriteLine("Total de elements en my_queue: {0}",my_queue.Count);
+
+            Console.WriteLine("VER el primer elemento de la cola: {0}",my_queue.Peek());
+
+            Console.WriteLine("Total de elements en my_queue: {0}", my_queue.Count);
+        }
+    }
+}
