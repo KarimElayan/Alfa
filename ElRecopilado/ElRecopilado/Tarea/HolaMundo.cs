d6ec9eeb1c37d899a8e6275920442f49316e8359
LAPTOP-NBQQP3J5\52771 <isafragoso862@gmail.com>
2021-01-26 20:37:54 -0600

    Practica 5 y 6

--- ElRecopilado/ElRecopilado/Tarea/HolaMundo.cs
+++ ElRecopilado/ElRecopilado/Tarea/HolaMundo.cs
@@ -11,7 +11,64 @@ namespace ElRecopilado.Tarea
             Console.WriteLine("Maria Isabel Fragoso Garcia");
             Console.WriteLine("CUH51197169");
             Console.WriteLine("cuh51197169@cuh.mx");
-              class Program
+        }
+
+            namespace Practica5
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int indiceFila, indiceCol, tamFilasCol;
+                Console.WriteLine("Matriz De usuario");
+                Console.WriteLine("Favor de usar un rango entre el 2 y el 100 para usar la matriz");
+                Console.WriteLine("y");
+                Console.WriteLine("Tambien usar un rango de numeros entre el -50 y el 50 para usar el programa");
+
+                Console.WriteLine("¿Cuantas Filas y Columnas tendra tu matriz?");
+                tamFilasCol = Convert.ToInt32(Console.ReadLine());
+
+
+                int[,] matrizUsuario = new int[tamFilasCol, tamFilasCol];
+
+                if (tamFilasCol >= 2 && tamFilasCol <= 100)
+                {
+
+                    for (indiceFila = 0; indiceFila < tamFilasCol; indiceFila++)
+                    {
+                        for (indiceCol = 0; indiceCol < tamFilasCol; indiceCol++)
+                        {
+                            Console.Write("Ingresa el dato del indice [" + indiceFila + "," + indiceCol + "]: ");
+                            matrizUsuario[indiceFila, indiceCol] = Convert.ToInt32(Console.ReadLine());
+
+                        }
+                    }
+
+
+                    Console.WriteLine("Los Datos que ingresaste son: ");
+                    for (indiceFila = 0; indiceFila < tamFilasCol; indiceFila++)
+                    {
+                        for (indiceCol = 0; indiceCol < tamFilasCol; indiceCol++)
+                        {
+                            Console.Write(matrizUsuario[indiceFila, indiceCol] + "," + " ");
+
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.Write("Numero Fuera de Rango. Solo Rangos del 2 al 100. Vuelve a Iniciar el programa.");
+                };
+
+
+
+            }
+        }
+    }
+
+
+    class Program
         {
             class Matriz
             {
