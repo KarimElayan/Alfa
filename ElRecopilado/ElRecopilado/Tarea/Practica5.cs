c85d7b0b07a80c3dbc85c13d28c73d070427d333
Usuario\Eduardo <cuh51187040@cuh.mx>
2021-01-28 22:47:56 -0600

    Practica 6

--- ElRecopilado/ElRecopilado/Tarea/Practica5.cs
+++ ElRecopilado/ElRecopilado/Tarea/Practica5.cs
@@ -10,6 +10,8 @@ namespace ElRecopilado.Tarea
         {
             int fila;
             int tamaño_linea, tamaño_fila;
+            int SumaDiagonalIzquierda = 0;
+            int SumaDiagonalDerecha = 0;
 
             Console.WriteLine("ingresar un numero entero entre el 2 y 100");
             linea = int.Parse(Console.ReadLine());
@@ -23,12 +25,36 @@ namespace ElRecopilado.Tarea
                 {
                     for (tamaño_fila = 0; tamaño_fila<fila; tamaño_fila++)
                     {
-                        matriz[tamaño_linea,tamaño_fila] = numero.Next(-50,50);
+                        matriz[tamaño_linea,tamaño_fila] = numero.Next(9);
                         Console.Write(" "+matriz[tamaño_linea, tamaño_fila]);
                     }
                     Console.WriteLine();
                 }
-                
+                for (int i = 0; i < linea; i++)
+                {
+                    for (int j = 0; j < linea; j++)
+                    {
+                        if (i == j)
+                        {Console.WriteLine();
+                            SumaDiagonalIzquierda = SumaDiagonalIzquierda + matriz [i,j]; 
+                           Console.WriteLine(""+matriz[i, j]);
+                           Console.WriteLine(SumaDiagonalIzquierda);
+                        }
+                    }
+                }
+
+                 for (int i = 0; i < linea; i++)
+                {
+                    for (int j = 0; j < linea; j++)
+                    {
+                        if (i + j == linea-1)
+                        {Console.WriteLine();
+                            SumaDiagonalDerecha = SumaDiagonalDerecha + matriz [i,j]; 
+                           Console.WriteLine(""+matriz[i, j]);
+                           Console.WriteLine(SumaDiagonalDerecha);
+                        }
+                    }
+                }
             }
             else
             {
