5c16a250feb614f2de571d4344b534fcf5966e9c
LAPTOP-A1BVGPE0\zoe_e <zoe_ene_pan@hotmail.com>
2021-01-29 12:28:00 -0600

    Edite el hola mundo

--- ElRecopilado/ElRecopilado/Tarea/HolaMundo.cs
+++ ElRecopilado/ElRecopilado/Tarea/HolaMundo.cs
@@ -8,8 +8,15 @@ namespace ElRecopilado.Tarea
     {
         public void HacerMagia()
         {
-            Console.WriteLine("Consuelo Yeriot Hernandez Martinez");
-            Console.WriteLine("CUH51187040");
+            Console.WriteLine("*************");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("_____________");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("ANGEL VICENTE MENDOZA MOARALES");
+            Console.WriteLine("..............................");
+            Console.WriteLine(":> 20 Años <:");
+            Console.WriteLine("(*o*) Grupo 19A (*o*)");
+            Console.WriteLine(" (T-T) CUH51197384 (T-T)");
         }
     }
 }
