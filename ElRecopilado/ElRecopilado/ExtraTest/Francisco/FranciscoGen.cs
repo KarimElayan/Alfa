c7f2d69786bae72a744289c600891f05057a79af
DESKTOP-CVL0AKA\Yair <cuh51197443@cuh.mx>
2021-02-09 11:09:15 -0600

    NT

---
+++ ElRecopilado/ElRecopilado/ExtraTest/Francisco/FranciscoGen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElRecopilado.ExtraTest.Francisco
+{
+    class FranciscoGen
+    {
+        public void Prueba()
+        {
+            Console.WriteLine("Prueba");
+        }
+    }
+}
