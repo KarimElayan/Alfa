2724f573d02e6559e5a431e8104d8e97cd919366
TOSHIBA-PC\TOSHIBA <danigomezdelgado@gmail.com>
2021-02-17 18:22:58 -0600

    extra

---
+++ ElRecopilado/ElRecopilado/extra/extra/extra/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace extra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] A = { 1, 2, 2, 3, 9, 5, 5, 4 };
+
+                int[] B = (from item in A
+                           group item by item into g
+                           where g.Count() == 1
+                           select g.Key).ToArray();
+
+                Console.WriteLine(string.Join(",", B.ToList()));
+            }
+        }
+    }
+}
+   
